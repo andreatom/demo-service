1496d727370e5fb2f124b2259c82f9ba05958bae
Andrea Toma <andreatoma309@gmail.com>
2026-07-16 10:38:37 +0200

    Modificata la classe Book per renderla entity

--- src/main/java/it/its/demo/demo_service/model/Book.java
+++ src/main/java/it/its/demo/demo_service/model/Book.java
@@ -1,14 +1,22 @@
 package it.its.demo.demo_service.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID) //Specifichiamo la strategia con cui è stato generato l'id
     String id;
+
     String name;
     String author;
     Integer quantity;
