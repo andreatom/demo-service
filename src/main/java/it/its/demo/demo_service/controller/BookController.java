7cbaa09a1078e0cde37a43cb5d180e6bc2f46077
Andrea Toma <andreatoma309@gmail.com>
2026-07-16 11:39:13 +0200

    Adeguamento BookController per lavorare con Jpa

--- src/main/java/it/its/demo/demo_service/controller/BookController.java
+++ src/main/java/it/its/demo/demo_service/controller/BookController.java
@@ -35,68 +35,68 @@ public class BookController {
         return bookService.insert(book);
     }
 
-    @PatchMapping("/{id}/v1")
-    @ResponseStatus(HttpStatus.OK)
-    public BookDto patch (
-            @PathVariable String id,
-            @RequestBody PatchBook patchBook
-    ) {
-        return bookService.patch(id, patchBook);
-    }
-
-    @PutMapping("/{id}/v1")
-    @ResponseStatus(HttpStatus.OK)
-    public BookDto patch (
-            @PathVariable String id,
-            @RequestBody InsertBook insertBook
-    ) {
-        return bookService.put(id, insertBook);
-    }
-
-    @GetMapping("/v1")
-    @ResponseStatus(HttpStatus.OK)
-    public List<BookDto> findAll(){
-        return bookService.findAll();
-    }
-
-    @GetMapping("/{id}/v1")
-    @ResponseStatus(HttpStatus.OK)
-    public BookDto findById(
-            @PathVariable String id
-    ){
-        return bookService.findById(id);
-    }
-
-    @GetMapping("/search/v1")
-    @ResponseStatus(HttpStatus.OK)
-    public List<BookDto> findByName(
-            @RequestParam String name
-    ){
-        return bookService.findByName(name);
-    }
-
-    @DeleteMapping("/{id}/v1")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void delete(
-            @PathVariable String id
-    ){
-       bookService.delete(id);
-    }
-
-    @PostMapping("/{id}/buy/v1")
-    @ResponseStatus(HttpStatus.OK)
-    public BookDto buy(
-            @PathVariable String id,
-            @Valid @RequestBody BuyRequest buyRequest
-    ){
-        return bookService.buy(id, buyRequest);
-    }
-
-    @GetMapping("/{id}/buy/total/v1")
-    @ResponseStatus(HttpStatus.OK)
-    public TransactionTotalDto transactionTotal(
-            @PathVariable String id
-    ){
-        return bookService.total(id);
-    }
+//    @PatchMapping("/{id}/v1")
+//    @ResponseStatus(HttpStatus.OK)
+//    public BookDto patch (
+//            @PathVariable String id,
+//            @RequestBody PatchBook patchBook
+//    ) {
+//        return bookService.patch(id, patchBook);
+//    }
+//
+//    @PutMapping("/{id}/v1")
+//    @ResponseStatus(HttpStatus.OK)
+//    public BookDto patch (
+//            @PathVariable String id,
+//            @RequestBody InsertBook insertBook
+//    ) {
+//        return bookService.put(id, insertBook);
+//    }
+//
+//    @GetMapping("/v1")
+//    @ResponseStatus(HttpStatus.OK)
+//    public List<BookDto> findAll(){
+//        return bookService.findAll();
+//    }
+//
+//    @GetMapping("/{id}/v1")
+//    @ResponseStatus(HttpStatus.OK)
+//    public BookDto findById(
+//            @PathVariable String id
+//    ){
+//        return bookService.findById(id);
+//    }
+//
+//    @GetMapping("/search/v1")
+//    @ResponseStatus(HttpStatus.OK)
+//    public List<BookDto> findByName(
+//            @RequestParam String name
+//    ){
+//        return bookService.findByName(name);
+//    }
+//
+//    @DeleteMapping("/{id}/v1")
+//    @ResponseStatus(HttpStatus.NO_CONTENT)
+//    public void delete(
+//            @PathVariable String id
+//    ){
+//       bookService.delete(id);
+//    }
+//
+//    @PostMapping("/{id}/buy/v1")
+//    @ResponseStatus(HttpStatus.OK)
+//    public BookDto buy(
+//            @PathVariable String id,
+//            @Valid @RequestBody BuyRequest buyRequest
+//    ){
+//        return bookService.buy(id, buyRequest);
+//    }
+//
+//    @GetMapping("/{id}/buy/total/v1")
+//    @ResponseStatus(HttpStatus.OK)
+//    public TransactionTotalDto transactionTotal(
+//            @PathVariable String id
+//    ){
+//        return bookService.total(id);
+//    }
 }
